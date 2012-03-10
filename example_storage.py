75ba8055689bb48a440c72f33256bf8724135305
msm <msm@Shuai-Mus-MacBook-Pro.local>
2012-03-10 22:43:27 +0800

    mlibcloud interface v1

--- example_storage.py
+++ example_storage.py
@@ -18,12 +18,52 @@ from pprint import pprint
 from libcloud.storage.types import Provider
 from libcloud.storage.providers import get_driver
 
+
+# Following is the original libcloud interface.
 CloudFiles = get_driver(Provider.CLOUDFILES_UK)
 
 driver = CloudFiles('access key id', 'secret key')
 
+driver.create_container("test_container")
+test_container = driver.get_container("test_container")
+temp_file = open("temp_file", "w")
+driver.upload_object("temp_file", test_container, "temp_object")
+temp_object = driver.get_object("test_container", "temp_object")
+driver.download_object(temp_object, "temp_file", True)
+driver.delete_object(obj)
+
 containers = driver.list_containers()
 container_objects = driver.list_container_objects(containers[0])
-
 pprint(containers)
 pprint(container_objects)
+
+driver.delete_container(test_container)
+
+# Following is the modified mlibcloud interface.
+# As seen, it is quite similar to the original interfaces.
+CloudFiles = get_driver(Provider.CLOUDFILES_UK)
+S3 = get_driver(Provider.S3_US_WEST)	
+GoogleStorage = get_driver(Provider.GOOGLE_STORAGE)
+
+driver_cf = CloudFiles('id', 'key')
+driver_s3 = S3('id', 'key')
+driver_gs = GoogleStorage('id', 'key')
+
+driver = GroupDriver([driver_cf, driver_s3, driver_gs])
+driver.set_original_share(2)	#set k
+driver.set_total_share(3)		#set m
+
+driver.create_container("test_container")
+test_container = driver.get_container("test_container")
+temp_file = open("temp_file", "w")
+driver.upload_object("temp_file", test_container, "temp_object")
+temp_object = driver.get_object("test_container", "temp_object")
+driver.download_object(temp_object, "temp_file", True)
+driver.delete_object(obj)
+
+containers = driver.list_containers()
+container_objects = driver.list_container_objects(containers[0])
+pprint(containers)
+pprint(container_objects)
+
+driver.delete_container(test_container)
