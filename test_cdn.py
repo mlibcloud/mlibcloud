797147cf8b4f7d7154e021ddbb3d05dfb05b2491
msmummy <msmummy@gmail.com>
2012-03-18 14:32:42 +0800

    test scripts

--- test_cdn.py
+++ test_cdn.py
@@ -3,6 +3,10 @@ from urllib2 import HTTPError
 import time
 from TestLogger import TestLogger
 import os
+from libcloud.common.types import LibcloudError
+from libcloud.storage.providers import get_driver
+from libcloud.storage.types import Provider, ContainerDoesNotExistError, ObjectHashMismatchError
+from RandomFile import RandomFile
 
 location = "Beijing"
 testid = 0;
@@ -15,41 +19,104 @@ while size <= 8 * 1024 * 1024:
 	azure_cdn_urls.append("%s%s" % (azure_cdn_base, size))
 	size *= 2
 
-if __name__ == "__main__":
-	
+s3_cdn_base = "http://d1dce9m55tbjje.cloudfront.net/Beijing_0_"
+size = 16 * 1024
+s3_cdn_urls=[]
+while size <= 8 * 1024 * 1024:
+	s3_cdn_urls.append("%s%s" % (s3_cdn_base, size))
+	size *= 2
 
+def test_cdn(server, cdn_base, cdn_urls):
+	up_down = "download"
+	for url in cdn_urls:
+		start_time = time.time()
+		end_time = 0
+		code = os.system("wget %s -O tempfile --timeout=10" % url)
+		if code == 0:
+			end_time = time.time()
+		else:
+			end_time = -1
+		file_size = url[len(cdn_base):]
+		TestLogger.getInstance().log(testid, location, server, start_time, end_time, file_size, up_down)
+
+def test_azure_cdn():
+	server = "CDN:Azure"
+	test_cdn(server, azure_cdn_base, azure_cdn_urls)
+
+def test_s3_cdn():
+	server = "CDN:S3"
+	test_cdn(server, s3_cdn_base, s3_cdn_urls)
+
+def test_s3_us_west():
+	S3_US_WEST = get_driver(Provider.S3_US_WEST)
+	driver_s3_us_west = S3_US_WEST("AKIAITLX6IDDU5VTNAPA", "Pi0BhJiVan/l6a2+Yg9JVxrNvZSTRMGIx39XWAGq");
+	driver = driver_s3_us_west
+	try:
+		container_s3_us_west = driver.get_container("mlibclouds3uswest")
+	except ContainerDoesNotExistError:
+		container_s3_us_west = driver.create_container("mlibclouds3uswest")
+	
+	#test for upload
+	f_group = RandomFile.create_group(location, testid)
+	for f_path in f_group:
+		start_time = time.time()
+		try:
+			driver.upload_object(f_path, container_s3_us_west, f_path, {"content_type": "application/octo-stream"})
+			end_time = time.time()
+		except LibcloudError, ObjectHashMismatchError:
+			end_time = -1
+		server = "original:S3_US_WEST"
+		file_size = f_path.split("_")[-1]
+		up_down = "upload"
+		TestLogger.getInstance().log(testid, location, server, start_time, end_time, file_size, up_down)
+	RandomFile.delete_group(f_group)
+	
+	#test for download
+	objects = driver.list_container_objects(container_s3_us_west)	
+
+	down_dic = {}
+	for o in objects:
+		fs_path = o.name			
+		info = fs_path.split("_")
+		#delete the object if it is generated locally 10 rounds ago
+		roundid = string.atoi(info[1])
+		loc = info[0]
+		if cmp(loc, location) == 0 and (testid - roundid) > 10:
+			conainer_s3_us_west.delete_object(o)
+		#prepare to pick a group to download
+		file_size = string.atoi(info[2])
+		if file_size == 8 * 1024 * 1024:
+			if down_dic.has_key(loc) and down_dic[loc] > roundid:
+				None
+			else:
+				down_dic[loc] = roundid
+
+	#prefer to download from other client uploads	
+	if len(down_dic) > 1 and down_dic.has_key(location):
+		down_dic.pop(location)
+	
+	info_tuple = down_dic.items()[1]	
+	to_down_objects = []
+	for o in objects:
+		info = fs_path.split("_")
+		loc = info[0]
+		roundid = string.atoi(info[1])
+		if cmp(loc, info_tuple[0]) and roundid == loc:
+			to_down_object.append(o)
+
+if __name__ == "__main__":
 	while True:
 		testid += 1	
 		
+		#test for cdns
 		#test for azure cdn	
-		server = "CDN:Azure"
-		up_down = "download"
-		for url in azure_cdn_urls:
-			try:
-				#print url
-				start_time = time.time()
-				end_time = 0;
-				code = os.system("wget %s -O tempfile --timeout=10" % url)	
-				#user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-				#headers = {'User-Agent' : user_agent}
-				#req = urllib2.Request(url, headers = headers)
-				#response = urllib2.urlopen(req)
-				#page = response.read()
-				#temp_f = open("tempfile", "w")
-				#temp_f.write(page)
-				#temp_f.close()
-				if code == 0:
-					end_time = time.time()
-				else :
-					end_time = -1
-				os.remove("tempfile");
-			except HTTPError:
-				end_time = -1
-			finally:
-				file_size = url[len(azure_cdn_base):]	
-				TestLogger.getInstance().log(testid, location, server, start_time, end_time, file_size, up_down)
-				
-		#test for original cloud
+#		test_azure_cdn()
 
+		#test for s3 cdn
+#		test_s3_cdn()
+
+		#test for original clouds
+		#test for s3_us_west()
+		test_s3_us_west()	
 
 		#test for mlibcloud
--- test_init_cdn.py
+++ test_init_cdn.py
@@ -1,13 +1,13 @@
 from libcloud.storage.providers import get_driver
-from libcloud.storage.types import Provider
+from libcloud.storage.types import Provider, ContainerDoesNotExistError, InvalidContainerNameError
 from pprint import pprint
 from RandomFile import RandomFile
 
-if __name__ == "__main__":
+def init_azure():
 	Azure = get_driver(Provider.WINDOWS_AZURE_STORAGE)
 	driver = Azure("mlibcloudcdn", "sE6yLNnYRjNM3vizbvbxfvEvDMUFEvtOfi01R2oZXH9NRfrh/ssOKfzMHNEoXhCqYaGnXMqWbJ7T+umzPWZGWA==")
 	
-	container_name = "mlibcloud"
+	container_name = "mlibcloudcdn"
 	if not driver.container_exists(container_name):
 		driver.create_container(container_name)
 	container = driver.get_container(container_name)
@@ -29,3 +29,24 @@ if __name__ == "__main__":
 	
 	
 	#driver.delete_container(container)
+	
+def init_s3():
+	S3 = get_driver(Provider.S3_AP_SOUTHEAST)
+	driver = S3("AKIAITLX6IDDU5VTNAPA", "Pi0BhJiVan/l6a2+Yg9JVxrNvZSTRMGIx39XWAGq")
+	container_name = "mlibcloudcdn"
+	try:
+		container = driver.get_container(container_name)
+	except ContainerDoesNotExistError:
+		try:
+			container = driver.create_container(container_name)
+		except InvalidContainerNameError:
+			print "Oh!"
+			exit()
+	f_group = RandomFile.create_group("Beijing", 0)
+	for f_path in f_group:
+		driver.upload_object(f_path, container, f_path, {"content_type":"application/octor-stream"})
+	RandomFile.delete_group(f_group)
+	
+if __name__ == "__main__":
+	#init_azure()
+	init_s3()
