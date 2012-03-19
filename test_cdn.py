246cd8cf4dbd2679717551b8e4ab9c5ffe60fc7d
msmummy <msmummy@gmail.com>
2012-03-19 15:46:17 +0800

    test scripts, almost done

--- test_cdn.py
+++ test_cdn.py
@@ -9,9 +9,40 @@ from libcloud.storage.types import Provider, ContainerDoesNotExistError, ObjectH
 from RandomFile import RandomFile
 import string
 import socket
+import random
+from GroupDriver import GroupDriver
+
 
 location = "Beijing"
 testid = 0;
+
+#drivers
+Ali = get_driver(Provider.ALIYUN_STORAGE)
+driver_ali = Ali("hqxxyywptpn3juer4zd5rods", "WfUMI6vw28r0GD4gwPtNRpS/unU=")
+
+Azure = get_driver(Provider.WINDOWS_AZURE_STORAGE)
+driver_azure_us = Azure("mlibcloud", "qdLKg2Cu1cWOkItbqb6gTl1WcOxvA9ED3fPo1KbSwdKw9ApJMhVEbyklurrBK23r8pTf6ajLN9tueSj5gVpiNQ==")
+
+GoogleStorage = get_driver(Provider.GOOGLE_STORAGE)
+driver_google_storage = GoogleStorage("GOOGULXCXRFPGQNEFPTE", "ys9om0uf2dYlXov4NOjO8jzGXLdtR7pwv9/nIK1V")
+
+S3_US_WEST = get_driver(Provider.S3_US_WEST)
+driver_s3_us_west = S3_US_WEST("AKIAITLX6IDDU5VTNAPA", "Pi0BhJiVan/l6a2+Yg9JVxrNvZSTRMGIx39XWAGq");
+
+NineFold = get_driver(Provider.NINEFOLD)
+driver_ninefold = NineFold("f9946e04515a46cf98a998f2cb34dd3b/mlibcloud_1328774465274", "fRRs33RyQOmVOrB38UNqV+R3uAM=");
+
+S3_AP_SOUTHEAST = get_driver(Provider.S3_AP_SOUTHEAST)
+driver_ap_southeast = S3_AP_SOUTHEAST("AKIAITLX6IDDU5VTNAPA", "Pi0BhJiVan/l6a2+Yg9JVxrNvZSTRMGIx39XWAGq");
+
+S3_AP_NORTHEAST = get_driver(Provider.S3_AP_NORTHEAST)
+driver_ap_southeast = S3_AP_NORTHEAST("AKIAITLX6IDDU5VTNAPA", "Pi0BhJiVan/l6a2+Yg9JVxrNvZSTRMGIx39XWAGq");
+
+#Cloudfiles_UK = get_driver(Provider.CLOUDFILES_UK)
+#driver_cloudfiles_uk = Cloudfiles_UK("mlibcloud0", "d544e3b4a183ba4d07777be6e6ce0b77z")
+
+Cloudfiles_US = get_driver(Provider.CLOUDFILES_US)
+driver_cloudfiles_us = Cloudfiles_US("mlibcloud", "5140858194409ed2dd2ec13e008ac754")
 
 #generate azure_cdn_urls
 azure_cdn_base = "http://az160243.vo.msecnd.net/mlibcloud/Beijing_0_"
@@ -98,8 +129,8 @@ def test_original(driver, container_name, server):
 	#prefer to download from other client uploads	
 	if len(down_dic) > 1 and down_dic.has_key(location):
 		down_dic.pop(location)
-	#TODO random	
-	info_tuple = down_dic.items()[0]	
+	#randomly pick a group to download
+	info_tuple = random.choice(down_dic.items())	
 	to_down_objects = []
 	
 	for o in objects:
@@ -127,52 +158,82 @@ def test_original(driver, container_name, server):
 	RandomFile.delete_group(f_group)
 
 def test_s3_us_west():
-	S3_US_WEST = get_driver(Provider.S3_US_WEST)
-	driver_s3_us_west = S3_US_WEST("AKIAITLX6IDDU5VTNAPA", "Pi0BhJiVan/l6a2+Yg9JVxrNvZSTRMGIx39XWAGq");
+	driver = driver_s3_us_west
 	server = "original:S3_US_WEST"
 	container_name = "mlibclouds3uswest"
-	test_original(driver_s3_us_west, container_name, server)
+	test_original(driver, container_name, server)
 
 def test_s3_ap_southeast():
-	S3_AP_SOUTHEAST = get_driver(Provider.S3_AP_SOUTHEAST)
-	driver = S3_AP_SOUTHEAST("AKIAITLX6IDDU5VTNAPA", "Pi0BhJiVan/l6a2+Yg9JVxrNvZSTRMGIx39XWAGq");
+	driver = driver_ap_southeast 
 	server = "original:S3_AP_SOUTHEAST"
 	container_name = "mlibclouds3apsoutheast"
 	test_original(driver, container_name, server)
 
 def test_s3_ap_northeast():
-	S3_AP_NORTHEAST = get_driver(Provider.S3_AP_NORTHEAST)
-	driver = S3_AP_NORTHEAST("AKIAITLX6IDDU5VTNAPA", "Pi0BhJiVan/l6a2+Yg9JVxrNvZSTRMGIx39XWAGq");
+	driver = driver_ap_northeast
 	server = "original:S3_AP_NORTHEAST"
 	container_name = "mlibclouds3apnortheast"
 	test_original(driver, container_name, server)
 
 def test_ali():
-	Ali = get_driver(Provider.ALIYUN_STORAGE)
-	driver = Ali("hqxxyywptpn3juer4zd5rods", "WfUMI6vw28r0GD4gwPtNRpS/unU=")
+	driver = driver_ali
 	server = "original:ALIYUN_STORAGE"
 	container_name = "mlibcloudali"
 	test_original(driver, container_name, server)
 
 def test_azure_us():
-	Azure = get_driver(Provider.WINDOWS_AZURE_STORAGE)
-	driver = Azure("mlibcloud", "qdLKg2Cu1cWOkItbqb6gTl1WcOxvA9ED3fPo1KbSwdKw9ApJMhVEbyklurrBK23r8pTf6ajLN9tueSj5gVpiNQ==")
-	server = "original:Azure"
+	driver = driver_azure_us
+	server = "original:AZURE"
 	container_name = "mlibcloudazure"
 	test_original(driver, container_name, server)
 
 def test_cloudfiles_us():
-	Cloudfiles_US = get_driver(Provider.CLOUDFILES_US)
-	driver = Cloudfiles_US("mlibcloud", "5140858194409ed2dd2ec13e008ac754")
-	server = "original:CloudFiles_US"
+	driver = driver_cloudfiles_us
+	server = "original:CLOUDFILES_US"
 	container_name = "mlibcloudcloudfilesus"
 	test_original(driver, container_name, server)
 
 def test_cloudfiles_uk():
-	Cloudfiles_UK = get_driver(Provider.CLOUDFILES_UK)
-	driver = Cloudfiles_UK("mlibcloud0", "d544e3b4a183ba4d07777be6e6ce0b77z")
-	server = "original:CloudFiles_UK"
+	driver = driver_cloudfiles_uk 
+	server = "original:CLOUDFILES_UK"
 	container_name = "mlibcloudcloudfilesuk"
+	test_original(driver, container_name, server)
+
+def test_google_storage():
+	driver = driver_google_storage
+	server = "original:GOOGLESTORAGE"
+	container_name = "mlibcloudgs"
+	test_original(driver, container_name, server)
+
+def test_ninefold():
+	driver = driver_ninefold 
+	server = "original:NINFOLD"
+	container_name = "mlibcloudninefold"
+	test_original(driver, container_name, server)
+
+
+def test_mlibcloud_3_7():
+	driver = GroupDriver([driver_ali, driver_azure_us, driver_google_storage, driver_s3_us_west, driver_cloudfiles_uk, driver_s3ap_southeast, driver_ninefold])
+	driver.set_original_share(3)
+	driver.set_total_share(7)
+	server = "mLibCloud:ALI$AZURE_US$GOOGLESTORAGE$S3_US_WEST$CLOUDFILES_UK$S3_AP_SOUTHEAST$NINEFOLD:3$7"
+	container_name = "mlibcloud37"
+	test_original(driver, container_name, server)
+
+def test_mlibcloud_3_6():
+	driver = GroupDriver([driver_ali, driver_azure_us, driver_google_storage, driver_s3_us_west, driver_cloudfiles_uk, driver_s3ap_southeast])
+	driver.set_original_share(3)
+	driver.set_total_share(6)
+	server = "mLibCloud:ALI$AZURE_US$GOOGLESTORAGE$S3_US_WEST$CLOUDFILES_UK$S3_AP_SOUTHEAST:3$6"
+	container_name = "mlibcloud36"
+	test_original(driver, container_name, server)
+
+def test_mlibcloud_3_5():
+	driver = GroupDriver([driver_ali, driver_azure_us, driver_google_storage, driver_s3_us_west, driver_cloudfiles_uk])
+	driver.set_original_share(3)
+	driver.set_total_share(6)
+	server = "mLibCloud:ALI$AZURE_US$GOOGLESTORAGE$S3_US_WEST$CLOUDFILES_UK:3$5"
+	container_name = "mlibcloud35"
 	test_original(driver, container_name, server)
 
 
@@ -189,18 +250,29 @@ if __name__ == "__main__":
 
 		#test for original clouds
 		#test for s3_us_west()
-		test_s3_us_west()	
+#		test_s3_us_west()	
 	
-		test_s3_ap_southeast()
+#		test_s3_ap_southeast()
 		
-		test_s3_ap_northeast()
+#		test_s3_ap_northeast()
 
-		test_ali()
+#		test_ali()
 
-		test_azure_us()
+#		test_azure_us()
 
-		test_cloudfiles_us()
+		#TODO
+#		test_cloudfiles_us()
 		
-		test_cloudfiles_uk()
+		#TODO
+#		test_cloudfiles_uk()
+
+#		test_google_storage()
+
+#		test_ninefold()
+	
+		#TODO
+#		test_mlibcloud_3_5()
+#		test_mlibcloud_3_6()
+#		test_mlibcloud_3_7()
 
 		#test for mlibcloud
